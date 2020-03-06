dd3594f9e26b8dfc282b2490a97e5dc61bf90bb9
Vashov <vashovway@gmail.com>
2020-03-06 11:37:43 +0700

    Тесты калькулятора счёта

--- UtilitiesBills.Tests/BillCalculatorServiceTest.cs
+++ UtilitiesBills.Tests/BillCalculatorServiceTest.cs
@@ -1,99 +1,88 @@
-﻿using System.Collections.Generic;
-using UtilitiesBills.Services.BillCalculator;
+﻿using UtilitiesBills.Services.BillCalculator;
 using Xunit;
 
 namespace UtilitiesBills.Tests
 {
     public class BillCalculatorServiceTest
     {
-        [Fact]
-        public void RoundCounterBulkTest()
+        [Theory]
+        [InlineData(0,   0)]
+        [InlineData(3,   3)]
+        [InlineData(3.4, 3)]
+        [InlineData(3.5, 4)]
+        [InlineData(3.6, 4)]
+        [InlineData(4.4, 4)]
+        [InlineData(4.5, 5)]
+        [InlineData(4.6, 5)]
+        public void RoundCounterBulkTest(decimal bulk, decimal expectedBulkAfterRound)
         {
             // Arrange
             var calculator = new BillCalculatorService();
-            var counterBulks = new List<decimal> { 0, 3, 3.4M, 3.45M, 3.5M, 3.6M, 4, 4.4M, 4.45M, 4.5M, 4.6M };
-            var expectBulks  = new List<decimal> { 0, 3, 3,    4,     4,    4,    4, 4,    5,     5,    5 };
-            var resultBulks = new List<decimal>();
 
             // Act
-            foreach (decimal bulk in counterBulks)
-            {
-                resultBulks.Add(calculator.RoundCounterBulk(bulk));
-            }
+            decimal resultBulkAfterRound = calculator.RoundCounterBulk(bulk);
 
             // Assert
-            for (int i = 0; i < expectBulks.Count; i++)
-            {
-                Assert.Equal(expectBulks[i], resultBulks[i]);
-            }
+            Assert.Equal(expectedBulkAfterRound, resultBulkAfterRound);
         }
 
-        [Fact]
-        public void RoundSumTest()
+        [Theory]
+        [InlineData(0,   0)]
+        [InlineData(3,   3)]
+        [InlineData(3.4, 3)]
+        [InlineData(3.5, 4)]
+        [InlineData(3.6, 4)]
+        [InlineData(4.4, 4)]
+        [InlineData(4.5, 5)]
+        [InlineData(4.6, 5)]
+        public void RoundSumTest(decimal sumBeforeRound, decimal expectedSumAfterRound)
         {
             // Arrange
             var calculator = new BillCalculatorService();
-            var beforeSums = new List<decimal> { 0, 3, 3.4M, 3.5M, 3.6M, 4, 4.4M, 4.5M, 4.6M };
-            var expectSums = new List<decimal> { 0, 3, 3,    4,    4,    4, 4,    5,    5 };
-            var resultSums = new List<decimal>();
 
             // Act
-            foreach (decimal sum in beforeSums)
-            {
-                resultSums.Add(calculator.RoundSum(sum));
-            }
+            decimal resultSum = calculator.RoundSum(sumBeforeRound);
 
             // Assert
-            for (int i = 0; i < expectSums.Count; i++)
-            {
-                Assert.Equal(expectSums[i], resultSums[i]);
-            }
+            Assert.Equal(expectedSumAfterRound, resultSum);
         }
 
-        [Fact]
-        public void CalcBulkExpenseTest()
+        [Theory]
+        [InlineData(0,   0,   0)]
+        [InlineData(3,   0,   0)]
+        [InlineData(0,   3,   0)]
+        [InlineData(2,   3,   6)]
+        [InlineData(3.4, 2,   6.8)]
+        [InlineData(3.5, 2.6, 9.1)]
+        public void CalcBulkExpenseTest(decimal bulk, decimal price, decimal expectExpenseResult)
         {
             // Arrange
             var calculator = new BillCalculatorService();
-            var bulks  = new List<decimal> { 0, 3, 0, 3.4M, 3.5M, 2 };
-            var prices = new List<decimal> { 0, 0, 3, 2,    2.6M, 3 };
-            var expect = new List<decimal> { 0, 0, 0, 6.8M, 9.1M, 6 };
-            var resultExpenses = new List<decimal>();
 
             // Act
-            for (int i = 0; i < bulks.Count; i++)
-            {
-                resultExpenses.Add(calculator.CalcBulkExpense(bulks[i], prices[i]));
-            }
+            decimal expenseResult = calculator.CalcBulkExpense(bulk, price);
 
             // Assert
-            for (int i = 0; i < expect.Count; i++)
-            {
-                Assert.Equal(expect[i], resultExpenses[i]);
-            }
+            Assert.Equal(expectExpenseResult, expenseResult);
         }
 
-        [Fact]
-        public void CalcBulkTest()
+        [Theory]
+        [InlineData(0,   0,  0)]
+        [InlineData(3,   2,  1)]
+        [InlineData(3,   3,  0)]
+        [InlineData(3.4, 2,  1.4)]
+        [InlineData(3.5, 2,  1.5)]
+        [InlineData(2,   3, -1)]
+        public void CalcBulkTest(decimal currBulk, decimal prevBulk, decimal expectedBulkResult)
         {
             // Arrange
             var calculator  = new BillCalculatorService();
-            var currBulks   = new List<decimal> { 0, 3, 3, 3.4M, 3.5M, 2 };
-            var prevBulks   = new List<decimal> { 0, 2, 3, 2,    2,    3 };
-            var expectBulks = new List<decimal> { 0, 1, 0, 1.4M, 1.5M, -1 };
-            var resultBulks = new List<decimal>();
 
             // Act
-            for (int i = 0; i < currBulks.Count; i++)
-            {
-                resultBulks.Add(calculator.CalcBulk(currBulks[i], prevBulks[i]));
-            }
+            decimal bulkResult = calculator.CalcBulk(currBulk, prevBulk);
 
             // Assert
-            for (int i = 0; i < expectBulks.Count; i++)
-            {
-                Assert.Equal(expectBulks[i], resultBulks[i]);
-            }
+            Assert.Equal(expectedBulkResult, bulkResult);
         }
     }
 }
