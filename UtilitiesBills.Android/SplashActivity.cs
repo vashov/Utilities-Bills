6709920153f693d84d17562fb365578c5227755a
Vashov <vashovway@gmail.com>
2020-03-17 00:32:57 +0700

    When app is starting, show splash screen

---
+++ UtilitiesBills.Android/SplashActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace UtilitiesBills.Droid
+{
+    [Activity(Theme = "@style/ThemeSplash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        // Prevent the back button from canceling the startup process
+        public override void OnBackPressed() { }
+    }
+}
